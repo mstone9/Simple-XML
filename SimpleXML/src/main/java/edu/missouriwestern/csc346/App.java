3c308071eb30f2e85158f7bb30244b0b945bddfe
Marshall Stone <99680411+mstone9@users.noreply.github.com>
2022-02-21 11:28:40 -0600

    Add files via upload

---
+++ SimpleXML/src/main/java/edu/missouriwestern/csc346/App.java
@@ -0,0 +1,13 @@
+package edu.missouriwestern.csc346;
+
+/**
+ * @Authors Marshall Stone, Cooper Jones, Gabriel Adams, Josh Mesmer
+ * @Since February 2022
+ */
+public class App 
+{
+    public static void main( String[] args ){
+
+
+    }
+}
