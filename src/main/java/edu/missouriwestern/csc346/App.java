aa698e2d50dd7cf84ef2252c02d52481a6a1a7c4
mstone9 <mstone9@missouriwestern.edu>
2022-02-21 12:21:57 -0600

    Starting the SimpleXML code

---
+++ src/main/java/edu/missouriwestern/csc346/App.java
@@ -0,0 +1,12 @@
+package edu.missouriwestern.csc346;
+
+/**
+ * @Authors Marshall Stone, Cooper Jones, Gabriel Adams, Josh Mesmer
+ * @Since February 2022
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println("Test");
+    }
+}
