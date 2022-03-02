44e1576d9eb911cdb17f40f9c6680510a6526d08
Marshall Stone <99680411+mstone9@users.noreply.github.com>
2022-03-02 13:37:59 -0600

    Finalization of the simple use case for Simple XML

--- src/main/java/edu/missouriwestern/csc346/NestedUseCase.java
+++ src/main/java/edu/missouriwestern/csc346/NestedUseCase.java
@@ -1,11 +1,14 @@
 package edu.missouriwestern.csc346;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.io.File;
 
 /**
  * @Authors Marshall Stone, Cooper Jones, Gabriel Adams, Josh Mesmer
  * @Since February 2022
  */
 public class NestedUseCase {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
     }
 }
