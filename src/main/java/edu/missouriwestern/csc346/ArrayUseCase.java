a511c710b09b2cd68bed7459e20d060c4939c39a
Cooper <cjones35@misosuriwestern.edu>
2022-03-04 19:28:01 -0600

    Completed NestedUseCase and nearly finished ArrayUseCase

--- src/main/java/edu/missouriwestern/csc346/ArrayUseCase.java
+++ src/main/java/edu/missouriwestern/csc346/ArrayUseCase.java
@@ -1,11 +1,102 @@
 package edu.missouriwestern.csc346;
 
+import org.simpleframework.xml.*;
+import org.simpleframework.xml.core.Persister;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Authors Marshall Stone, Cooper Jones, Gabriel Adams, Josh Mesmer
  * @Since February 2022
  */
-public class ArrayUseCase {
+public class ArrayUseCase{
     public static void main(String[] args){
+        try{
+            Serializer serializer = new Persister();
+            File file = new File("states.xml");
 
+            States states = serializer.read(States.class, file);
+
+            System.out.println(Arrays.toString(states.getStates()));
+        }catch(Exception e){
+            System.out.println("Exception: " + e.getMessage());
+            System.out.println("Cause: " + e.getCause());
+        }
+    }
+}
+
+@Root
+class States{
+    @ElementList(inline = true)
+    private List<State> states;
+
+    public Object[] getStates(){return states.toArray();}
+}
+
+@Root
+class State{
+    @Element
+    private String state;
+    @Element
+    private String slug;
+    @Element
+    private String code;
+    @Element
+    private String nickname;
+    @Element
+    private String website;
+    @Element
+    private String admission_date;
+    @Element
+    private int admission_number;
+    @Element
+    private String capital_city;
+    @Element
+    private String capital_url;
+    @Element
+    private int population;
+    @Element
+    private int population_rank;
+    @Element
+    private String constitution_url;
+    @Element
+    private String state_flag_url;
+    @Element
+    private String state_seal_url;
+    @Element
+    private String map_image_url;
+    @Element
+    private String landscape_background_url;
+    @Element
+    private String skyline_background_url;
+    @Element(required = false)
+    private String twitter_url;
+    @Element(required = false)
+    private String facebook_url;
+
+    @Override
+    public String toString() {
+        return "State{" +
+                "state='" + state + '\'' +
+                ", slug='" + slug + '\'' +
+                ", code='" + code + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", website='" + website + '\'' +
+                ", admission_date='" + admission_date + '\'' +
+                ", admission_number=" + admission_number +
+                ", capital_city='" + capital_city + '\'' +
+                ", capital_url='" + capital_url + '\'' +
+                ", population=" + population +
+                ", population_rank=" + population_rank +
+                ", constitution_url='" + constitution_url + '\'' +
+                ", state_flag_url='" + state_flag_url + '\'' +
+                ", state_seal_url='" + state_seal_url + '\'' +
+                ", map_image_url='" + map_image_url + '\'' +
+                ", landscape_background_url='" + landscape_background_url + '\'' +
+                ", skyline_background_url='" + skyline_background_url + '\'' +
+                ", twitter_url='" + twitter_url + '\'' +
+                ", facebook_url='" + facebook_url + '\'' +
+                '}';
     }
 }
--- src/main/java/edu/missouriwestern/csc346/NestedUseCase.java
+++ src/main/java/edu/missouriwestern/csc346/NestedUseCase.java
@@ -1,8 +1,11 @@
 package edu.missouriwestern.csc346;
+import org.simpleframework.xml.*;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 /**
  * @Authors Marshall Stone, Cooper Jones, Gabriel Adams, Josh Mesmer
@@ -10,5 +13,91 @@ import java.io.File;
  */
 public class NestedUseCase {
     public static void main(String[] args) {
+        Serializer serializer = new Persister();
+
+        try{
+            URL url = new URL("https://w1.weather.gov/xml/current_obs/KSTJ.xml");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            Weather weather = serializer.read(Weather.class, in);
+            in.close();
+
+
+            System.out.println(weather.toString());
+        }catch(Exception e){
+            System.out.println("Exception: " + e.getMessage());
+            System.out.println("Cause: " + e.getCause());
+        }
     }
+}
+
+@Root(strict = false, name="curent_observation")
+class Weather{
+    @Attribute
+    private double version;
+
+    @Element
+    private String credit;
+    @Element
+    private String credit_URL;
+    @Element
+    private Image image;
+    @Element
+    private String location;
+    @Element
+    private String station_id;
+    @Element
+    private String observation_time;
+    @Element
+    private String observation_time_rfc822;
+    @Element
+    private String weather;
+    @Element
+    private double temp_f;
+    @Element
+    private double temp_c;
+
+    public double getVersion(){return version;}
+    public String getCredit(){return credit;}
+    public String getCredit_URL(){return credit_URL;}
+    public String getImageUrl(){return image.getUrl();}
+    public String getImageTitle(){return image.getTitle();}
+    public String getImageLink(){return image.getLink();}
+    public String getLocation(){return location;}
+    public String getStation_id(){return station_id;}
+    public String getObservation_time(){return observation_time;}
+    public String getObservation_time_rfc822(){return observation_time_rfc822;}
+    public String getWeather(){return weather;}
+    public double getTemp_f(){return temp_f;}
+    public double getTemp_c(){return temp_c;}
+
+    @Override
+    public String toString(){
+        return "Weather{" +
+                "version=" + version +
+                ", credit='" + credit + '\'' +
+                ", credit_URL='" + credit_URL + '\'' +
+                ", image=" + image +
+                ", location='" + location + '\'' +
+                ", station_id='" + station_id + '\'' +
+                ", observation_time='" + observation_time + '\'' +
+                ", observation_time_rfc822='" + observation_time_rfc822 + '\'' +
+                ", weather='" + weather + '\'' +
+                ", temp_f=" + temp_f +
+                ", temp_c=" + temp_c +
+                '}';
+    }
+}
+
+class Image{
+    @Element
+    private String url;
+    @Element
+    private String title;
+    @Element
+    private String link;
+
+    public String getUrl(){return url;}
+    public String getTitle(){return title;}
+    public String getLink(){return link;}
 }
